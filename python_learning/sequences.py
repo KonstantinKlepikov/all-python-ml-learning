161b145bd1e004aabfc00e1b143fb5847721a334
KonstantinKlepikov <oformleno@gmail.com>
2020-07-09 02:49:58 +0200

    python learning

--- python_learning/sequences.py
+++ python_learning/sequences.py
@@ -1,21 +1,22 @@
 from collections import deque
+import heapq
 
-def arbitrary_len (some_seq, form=None):
+def arbitrary_len (some_seq, mode=None):
     """Function crops sequences, leaving a first, middle or last part
 
     Args:
         some_seq (sequence): sequence of any type
-        form (string, None): "firt", "middle", "last". Defaults to None.
+        mode (string, optional): "firt", "middle", "last". Defaults to None.
 
     Returns:
-        [object]: elements of cropped sequence
+        obj: element or list of elements of cropped sequence
     """
-    
-    if form == "first":
+
+    if mode == "first":
         answer, *_ = some_seq
-    elif form == "middle":
+    elif mode == "middle":
         _, *answer, _ = some_seq
-    elif form == "last":
+    elif mode == "last":
         *_, answer = some_seq
 
     return answer
@@ -25,11 +26,11 @@ def search_and_scrop(some_seq, pattern, N=5):
 
     Args:
         some_seq (sequence): sequence of any type
-        pattern (obj): object same type, as sequence elements
+        pattern (obj, optional): object same type, as sequence elements
         N (int): Number of last elements. Defaults to 5.
 
     Yields:
-        obj: N previous elements of sequence
+        list: N previous elements of sequence
     """
 
     prev_elem = deque(maxlen=N)
@@ -37,3 +38,23 @@ def search_and_scrop(some_seq, pattern, N=5):
         if pattern in elem:
             yield elem, prev_elem
         prev_elem.append(elem)
+
+def searc_for_minmax(some_seq, mode=None, key=None, N=3):
+    """Serach smallest or largest elements in smll sets of data with heapq
+
+    Args:
+        some_seq (sequence): sequence of any type
+        mode (str, optional): "min", "max". Defaults to None.
+        key (func, optional): function to sort option. Defaults to None.
+        N (int, optional): Number of searched elements. Defaults to 3.
+
+    Returns:
+        list: list of N objects
+    """
+
+    if mode == "min":
+        answer = heapq.nsmallest(N, some_seq, key=key)
+    elif mode == "max":
+        answer = heapq.nlargest(N, some_seq, key=key)
+    
+    return answer
