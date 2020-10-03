1d7d3c23306fbeb7ac53588bae012e0172d1e3e8
KonstantinKlepikov <oformleno@gmail.com>
2020-10-03 03:48:50 +0200

    python learning oop

--- python_learning/slots_example.py
+++ python_learning/slots_example.py
@@ -25,7 +25,26 @@ class E:
     c = 3 # class attribute is fine
 
     def __init__(self):
-        self.d = 4 # d is storaged in __dict__, but is a slot 
+        self.d = 4 # d is storaged in __dict__, but is a slot
+
+
+# only last slots is visible, other is hidden
+class First:
+
+    __slots__ = ['c', 'd']
+
+class Second(First):
+
+    __slots__ = ['a', '__dict__']
+
+
+class Slotful:
+
+    __slots__ = ['a', 'b', '__dict__']
+
+    def __init__(self, data):
+        self.c = data
+
 
 if __name__ == "__main__":
 
@@ -72,3 +91,21 @@ if __name__ == "__main__":
     print(v.__dict__) # {'d': 4}
     print(v.__slots__) # {'a', 'b', '__dict__'}
     print(getattr(v, 'a'), getattr(v, 'c'), getattr(v, 'd')) # (1, 3, 4)
+
+
+    fs = Second() # in exemplar we have all attributes (because __dict__), but not all slots
+    fs.a = 1
+    fs.b = 2
+    fs.c = 3
+    print(fs.a, fs.b, fs.c)
+    print(First.__slots__) # [’с’, ’d’]
+    print(Second.__slots__) # [’а', ’__diet__’]
+    print(fs.__slots__) # [' а', '__dict__'] - inheritance last
+    print(fs.__dict__) # {'Ь': 2}
+
+
+    I = Slotful(3)
+    I.a = 1
+    I.b = 2
+    print(I.a, I.b, I.c)
+    print(I.__dict__)
