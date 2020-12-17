38a412d25ab04118ee4d7200723b02b887393cc8
KonstantinKlepikov <oformleno@gmail.com>
2020-12-17 03:26:19 +0200

    string bases

--- python_learning/string_base.py
+++ python_learning/string_base.py
@@ -72,3 +72,68 @@ print(s.encode('latin-1'), len(s.encode('latin-1'))) # b'\xc4\xe8' 2
 print(s.encode('utf-8'), len(s.encode('utf-8'))) # b'\xc3\x84\xc3\xa8' 4
 
 # for other types of coding/decoding look in book
+
+# bytes literals
+
+# str can detect 16-th and unicode
+s = 'A\xC4B\xE8C'
+su = 'A\u00C4B\U000000E8C'
+print(s, su) # AÄBèC AÄBèC
+
+# bytes can detect 16-th, but cant unicode
+b = b'A\xC4B\xE8C'
+bu = b'A\u00C4B\U000000E8C'
+print(b, bu) # b'A\xc4B\xe8C' b'A\\u00C4B\\U000000E8C'
+
+# transformation between encodings
+
+b = b'A\xc3\x84B\xc3\xa8C' # utf-8
+s = b.decode('utf-8') # unicode in utf-8
+print(s) # AÄBèC
+t = s.encode('cp500') # bytes in EBCDIC
+print(t) # b'\xc1c\xc2T\xc3'
+u = t.decode('cp500') # unicode with EBCDIC
+print(u) # AÄBèC
+print(u.encode()) # b'A\xc3\x84B\xc3\xa8C' bytes with utf-8
+
+# bytes objects and bytes attributes
+
+# attributes
+print(set(dir('abc')) - set(dir(b'abc')))
+# {'format_map', 'casefold', 'encode', 'isprintable', 'isdecimal', 'isidentifier', 'format', 'isnumeric'}
+
+print(set(dir(b'abc')) - set(dir('abc')))
+# {'decode', 'hex', 'fromhex'}
+
+# other methods for bytes generation
+
+b = b'abc' # literal
+b = bytes('abc', 'ascii') # constructor with encoding definition
+b = bytes([97, 98, 99]) # iterated object with whole numbers
+b = 'spam'.encode() # str.encode()
+
+# bytearray
+
+s = 'spam'
+c = bytearray(s, 'utf-8')
+print(c) # bytearray(b'spam')
+b = b'spam'
+c = bytearray(b)
+print(c) # bytearray(b'spam')
+
+print(c[0])
+c[0] = ord('x') # we can use only ordinal value 0-255 ASCII
+print(c) # bytearray(b'xpam')
+
+# attributes
+print(set(dir('abc')) - set(dir(bytearray(b'abc'))))
+# {'casefold', 'format_map', 'isprintable', 'isnumeric', 'isdecimal', 'format', '__getnewargs__', 'encode', 'isidentifier'}
+
+print(set(dir(bytearray(b'abc'))) - set(dir('abc')))
+# {'append', '__iadd__', 'fromhex', 'extend', 'reverse', '__setitem__', '__delitem__', '__imul__', 'hex', '__alloc__', 'clear', 'remove', 'decode', 'insert', 'copy', 'pop'}
+
+c.append(ord('L'))
+print(c) # bytearray(b'xpamL')
+c.extend(b'MNO')
+print(c) # bytearray(b'xpamLMNO')
+# and etc...
