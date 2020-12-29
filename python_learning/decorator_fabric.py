cd19c1098b0a7d458fb0adac3f78625e00b1172c
KonstantinKlepikov <oformleno@gmail.com>
2020-12-29 23:23:36 +0200

    decorators attributes

--- python_learning/decorator_fabric.py
+++ python_learning/decorator_fabric.py
@@ -1,4 +1,4 @@
-# fabric method for decorators
+# fabric method for decorators (isnt best idea)
 
 from abc import ABC, abstractmethod
 
@@ -7,48 +7,52 @@ class Fabricator(ABC):
 
     @classmethod
     @abstractmethod
-    def _is_check_for(cls, check):
+    def is_check_for(cls, check):
         pass
 
     @abstractmethod
-    def _do_something(self):
+    def do_something(self):
         pass
 
 
 class Product1(Fabricator):
 
     @classmethod
-    def _is_check_for(cls, check):
+    def is_check_for(cls, check):
         return check == 1
 
-    def _do_something(self):
+    def do_something(self):
         print('Now im in Product1')
 
 
 class Product2(Fabricator):
 
     @classmethod
-    def _is_check_for(cls, check):
+    def is_check_for(cls, check):
         return check == 2
 
-    def _do_something(self):
+    def do_something(self):
         print('Now im in Product2')
 
 class tracker:
+    """Im tracker docstring
+    """
 
     def __init__(self, function):
         self.function = function
 
     def __call__(self, check, *args):
-
         for cls_ in Fabricator.__subclasses__():
-            if cls_._is_check_for(check):
-                cls_()._do_something()
+            if cls_.is_check_for(check):
+                cls_().do_something()
         self.function(check, *args)
 
 
 @tracker
 def tester(check, a, b):
+    """Im tester docstring
+    """
+
     print('Sum is: ', a + b)
 
 
@@ -56,3 +60,5 @@ if __name__ == "__main__":
 
     for i in 1, 2:
         tester(i, 1, 2)
+    
+    print(tester.__doc__)
--- python_learning/decorators_base.py
+++ python_learning/decorators_base.py
@@ -42,12 +42,78 @@ def eggs(x, y):
     print(x ** y)
 
 
+# state from enclosing area of vision and global variable
+
+calls = 0 # is global and is changed by each call
+
+def tracerEnc(func):
+
+    def wrapper(*args, **kwargs):
+        global calls
+        calls += 1
+        print('call {0} to {1}'.format(calls, func.__name__))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@tracerEnc
+def spam2(a, b, c):
+    print(a + b + c)
+
+@tracerEnc
+def eggs1(x, y):
+    print(x ** y)
+
+
+# state from enclosing area of vision and nonlocal variable
+
+def tracerEncNonloc(func):
+
+    calls = 0 # is nonlocal and work as class attribute
+
+    def wrapper(*args, **kwargs):
+        nonlocal calls
+        calls += 1
+        print('call {0} to {1}'.format(calls, func.__name__))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@tracerEncNonloc
+def spam3(a, b, c):
+    print(a + b + c)
+
+@tracerEncNonloc
+def eggs2(x, y):
+    print(x ** y)
+
+
+# function attributes
+def tracerAttrFunc(func):
+
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        print('call {0} to {1}'.format(wrapper.calls, func.__name__))
+        return func(*args, **kwargs)
+
+    wrapper.calls = 0
+    return wrapper
+
+@tracerAttrFunc
+def spam4(a, b, c):
+    print(a + b + c)
+
+@tracerAttrFunc
+def eggs3(x, y):
+    print(x ** y)
+
 if __name__ == "__main__":
 
     spam(1, 2, 3)
     spam('a', 'b', 'c')
     spam.calls
     print(spam)
+    print('=' * 50)
 
 
     # attributes of class instances
@@ -55,3 +121,26 @@ if __name__ == "__main__":
     spam1(a=4, b=5, c=6)
     eggs(2, 16)
     eggs(2, y=4)
+    print('=' * 50)
+
+    # enclousing area global
+    spam2(1, 2, 3)
+    spam2(a=4, b=5, c=6)
+    eggs1(2, 16)
+    eggs1(2, y=4)
+    print('=' * 50)
+
+    # enclousing area nonlocal
+    spam3(1, 2, 3)
+    spam3(a=4, b=5, c=6)
+    eggs2(2, 16)
+    eggs2(2, y=4)
+    print('=' * 50)
+
+    # function attributes
+    spam4(1, 2, 3)
+    spam4(a=4, b=5, c=6)
+    eggs3(2, 16)
+    eggs3(2, y=4)
+    print('=' * 50)
+
